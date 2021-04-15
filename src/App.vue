f090594889c1790c123026799ae53ab1e14fa280
Miguel Arias <ctdesing@gmail.com>
2021-04-15 11:52:06 -0400

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,3 @@
 <template>
-  <h1>Hello App</h1>
-  <button class='bg-green-500'>clicked</button>
+  <h1>Hello World</h1>
 </template>
-
-<script setup>
-
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from 'vite';
+
+import vue from '@vitejs/plugin-vue';
+
+import path from 'path';
+
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()]
-})
+
+  plugins: [vue()],
+  // alias: {
+  //   '/@/': './src',
+  // },
+  resolve: {
+    alias: [
+      {
+        find: 'src',
+        replacement: path.resolve(__dirname, './src'),
+      },
+      {
+        find: 'components',
+        replacement: path.resolve(__dirname, './src/components'),
+      },
+      {
+        find: 'public',
+        replacement: path.resolve(__dirname, './public'),
+      },
+    ],
+  },
+  optimizeDeps: {
+    include: ['lodash'],
+  },
+
+});
+
