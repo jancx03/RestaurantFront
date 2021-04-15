2f37342ba1f2817bac8b4f920dba4cdee7c109b5
Miguel Arias <ctdesing@gmail.com>
2021-04-15 16:55:40 -0400

    new Vuex Store

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+
+const state = () => {};
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+export default createStore({
+  state,
+  getters,
+  actions,
+  mutations,
+});
