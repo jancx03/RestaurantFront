15df1cb250eba9cf146fd326362b72e86b8cddf4
Miguel Arias <ctdesing@gmail.com>
2021-04-15 17:13:04 -0400

    TheInputBar component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Hello World</h1>
+  <TheInputbar />
 </template>
+
+<script>
+import TheInputbar from 'components/TheInputBar.vue';
+
+export default {
+  components: { TheInputbar },
+};
+</script>
+
+<style>
+* {
+  font-size: 65%;
+  font-family: 'Pacifico', cursive;
+}
+body {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/TheInputBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <input class="input" type="text">
+  </div>
+</template>
+
+<style scoped>
+input {
+  width: 50vw;
+  border-radius: 5rem;
+  height: 3rem;
+  background-color: aliceblue;
+}
+
+input:focus {
+  background-color: plum;
+}
+</style>
