2a55855372d3436d9e53872eaa7b6ef28a12584f
Jhon Nieves <jancx031@gmail.com>
2021-04-15 19:33:46 -0400

    component

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,27 @@
 </template>
 
 <script>
-import TheInputbar from 'components/TheInputBar.vue';
+import TheInputbar from "components/TheInputBar.vue";
+import test from "./components/test.vue";
 
 export default {
-  components: { TheInputbar },
+  components: { TheInputbar, test },
+  data() {
+    return {
+      user: {
+        name: "Monkey D. Luffy",
+        age: 17,
+        power: "Gomu Gomu no Mi",
+      },
+    };
+  },
 };
 </script>
 
 <style>
 * {
   font-size: 65%;
-  font-family: 'Pacifico', cursive;
+  font-family: "Pacifico", cursive;
 }
 body {
   box-sizing: border-box;
---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h2>{{ charname }}</h2>
+    <h3>{{ charage }} Years</h3>
+    <h3>{{ charpower }} Years</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+    },
+    userage: {
+      type: String,
+    },
+    usermail: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
