c6ebeb188d63b3ff9b494b945165ed00bd700bcb
Jhon Nieves <jancx031@gmail.com>
2021-05-04 17:08:55 -0400

    bar

--- src/components/TheInputBar.vue
+++ src/components/TheInputBar.vue
@@ -1,5 +1,24 @@
 <template>
-<div class="bg-blue-500">This is checking</div>
+  <form action="">
+  <div class="flex justify-center pt-14 pr-20 pb-13">
+    <div>
+      <span class="mr-12 text-gray-600">Find</span>
+      <span class="">
+        <input type="text" placeholder="restaurants" class="font-normal" />
+      </span>
+    </div>
+    <div class="font-medium">
+      <label for="near" >Near</label>
+      <input
+        type="text"
+        placeholder="address, neighborhood, city, state or zip"
+      />
+    </div>
+    <div class="">
+      <button class="bg-red-700" >search</button>
+    </div>
+  </div>
+  </form>
 </template>
 
 <script>
@@ -22,5 +41,4 @@ export default {
 </script>
 
 <style>
-
 </style>
