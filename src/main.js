dcd877e025034b91e6df8b21118aa33d52023a51
Miguel Arias <ctdesing@gmail.com>
2021-05-04 15:41:00 -0400

    vuetify removal

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue';
 import router from '@/router.js';
 import store from './store';
-import vuetify from './plugins/vuetify';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(store);
-app.use(vuetify);
 app.use(router);
 
 app.mount('#app');
