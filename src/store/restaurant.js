8ea3cc533a1ce5a11ef2d8dd9211342817f8163b
Miguel Arias <ctdesing@gmail.com>
2021-05-04 15:23:21 -0400

    restaurant store

--- src/store/restaurant.js
+++ src/store/restaurant.js
@@ -5,5 +5,45 @@ const namespaced = true;
 const state = () => ({
   restaurants: [],
   restaurant: {},
-  search: '',
+  error: null,
 });
+
+const getters = {
+  restaurants: (state) => state.restaurants,
+  restaurant: (state) => state.restaurant,
+  error: (state) => state.error,
+};
+
+const mutations = {
+  setRestaurants: (state, values) => state.restaurants = values,
+  setRestaurant: (state, value) => state.restaurant = value,
+  error: (state, value) => state.error = value,
+};
+
+const actions = {
+  queryRestaurants: async (context) => {
+    try {
+      const response = await fetch('https://d1o8lt9womy1vs.cloudfront.net/restaurants', {
+        Headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      const result = response.json();
+      const restaurants = await result;
+      context.commit('restaurants', restaurants);
+      return 0;
+    } catch (err) {
+      context.commit('error', err);
+      return 1;
+    }
+  },
+};
+
+export default {
+  namespaced,
+  state,
+  getters,
+  mutations,
+  actions,
+};
