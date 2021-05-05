ee129b52a73df1840fb1b24cf473e7f91d62bb9d
Jhon Nieves <jancx031@gmail.com>
2021-05-05 19:40:48 -0400

    background

--- src/pages/TheIndex.vue
+++ src/pages/TheIndex.vue
@@ -1,5 +1,10 @@
 <template>
-  <TheSearchBar />
+<div class="image">
+</div>
+  <div class="container">
+      <TheSearchBar />
+  </div>
+
 </template>
 
 <script>
@@ -13,3 +18,17 @@ export default {
   },
 };
 </script>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  margin-top: 10rem;
+}
+.image {
+  background-image: url("https://images.unsplash.com/photo-1525648199074-cee30ba79a4a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80");
+  /* background-color: #cccccc; */
+  width: 100%;
+  height: 100%;
+}
+
+</style>
