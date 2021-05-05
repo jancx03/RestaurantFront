d91dd4f50493f3259680184a4b2de2a7ee8909c9
Jhon Nieves <jancx031@gmail.com>
2021-05-05 19:55:24 -0400

    nothing

--- src/pages/TheIndex.vue
+++ src/pages/TheIndex.vue
@@ -1,6 +1,8 @@
 <template>
-<div class="image">
+<div>
+
 </div>
+
   <div class="container">
       <TheSearchBar />
   </div>
@@ -27,8 +29,7 @@ export default {
 .image {
   background-image: url("https://images.unsplash.com/photo-1525648199074-cee30ba79a4a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80");
   /* background-color: #cccccc; */
-  width: 100%;
-  height: 100%;
+
 }
 
 </style>
