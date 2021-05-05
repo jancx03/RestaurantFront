ef938f6258671b0fcad94d6c485308b4f780c48a
Miguel Arias <ctdesing@gmail.com>
2021-05-05 19:41:23 -0400

    skeleton

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 * {
   font-size: 100%;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
+  margin: 0;
+  padding: 0;
 }
 body {
   box-sizing: border-box;
--- src/pages/TheIndex.vue
+++ src/pages/TheIndex.vue
@@ -1,13 +1,15 @@
 <template>
   <TheSearchBar />
+  <Skeleton />
 </template>
 
 <script>
 import TheSearchBar from 'components/TheInputBar.vue';
+import Skeleton from 'components/Skeleton.vue';
 import { mapActions } from 'vuex';
 
 export default {
-  components: { TheSearchBar },
+  components: { TheSearchBar, Skeleton },
   methods: {
     ...mapActions('restaurantStore', ['queryRestaurants']),
   },
