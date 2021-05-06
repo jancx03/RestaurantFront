f06e4d559cbd8602661a6ff13b9a177b09073715
Jhon Nieves <jancx031@gmail.com>
2021-05-05 20:25:25 -0400

    d

--- src/pages/TheIndex.vue
+++ src/pages/TheIndex.vue
@@ -25,7 +25,9 @@ export default {
   background-image: url("https://images.unsplash.com/photo-1525648199074-cee30ba79a4a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80");
   width: 100;
   height: 100vh;
+  background-repeat: no-repeat;
   display: flex;
+  background-position: left top;
   justify-content: center;
   align-items: center;
   /* margin-top: 10rem; */
