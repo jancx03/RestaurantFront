cfa708fec3a7975b65ac6f8a05226521bc0ed8c2
Miguel Arias <ctdesing@gmail.com>
2021-05-05 20:22:39 -0400

    awaiting pull

--- src/pages/TheIndex.vue
+++ src/pages/TheIndex.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="image">
     <TheSearchBar />
-    <Skeleton />
   </div>
 </template>
 
 <script>
 import TheSearchBar from 'components/TheInputBar.vue';
-import Skeleton from 'components/Skeleton.vue';
 import { mapActions } from 'vuex';
 
 export default {
-  components: { TheSearchBar, Skeleton },
+  components: { TheSearchBar },
   methods: {
     ...mapActions('restaurantStore', ['queryRestaurants']),
   },
@@ -22,10 +20,10 @@ export default {
   background-image: url("https://images.unsplash.com/photo-1525648199074-cee30ba79a4a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80");
   /* background-color: #cccccc; */
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
-  margin-top: 10rem;
+  align-items: center;
 }
 
 </style>
